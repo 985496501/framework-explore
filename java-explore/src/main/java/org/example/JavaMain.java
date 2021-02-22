88e1f67fbedb602ba4e40243255e47ee45e0c72e
jinyun <985496501@qq.com>
2021-02-22 15:40:01 +0800

    优化代码2

--- java-explore/src/main/java/org/example/JavaMain.java
+++ java-explore/src/main/java/org/example/JavaMain.java
@@ -4,7 +4,8 @@ package org.example;
  * 假期期间完成 jvm内存相关的系统学习
  * 阿里P7 技术专家 硬性条件
  *
- *
+ * 使用maven构建项目：
+ * http://www.choupangxia.com/2020/12/01/maven-optional-scope/
  *
  * @author: jinyun
  * @date: 2021/2/1
